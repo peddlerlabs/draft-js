ffc9fe359d1b4cc1b4028d1ef96848a98c9b5a65
KhoaNguyen <khoanguyendnvn@gmail.com>
2017-06-09 14:52:29 +0700

    explicitly bind the context to setImmediate

--- src/component/handlers/edit/editOnBeforeInput.js
+++ src/component/handlers/edit/editOnBeforeInput.js
@@ -172,7 +172,7 @@ function editOnBeforeInput(editor: DraftEditor, e: SyntheticInputEvent): void {
     // before we actually update our state. That way when we rerender, the text
     // we see in the DOM will already have been inserted properly.
     editor._pendingStateFromBeforeInput = newEditorState;
-    setImmediate(() => {
+    setImmediate.bind(this, function () {
       if (editor._pendingStateFromBeforeInput !== undefined) {
         editor.update(editor._pendingStateFromBeforeInput);
         editor._pendingStateFromBeforeInput = undefined;
